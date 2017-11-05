ea6806ffab26ab993c5aff4ebda1e2609c480387
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-05 21:56:44 +0000

    Added MongoDB configuration to app.

--- my_app.rb
+++ my_app.rb
@@ -1,9 +1,11 @@
 require './lib/swaggering'
+require 'mongoid'
 
 # only need to extend if you want special configuration!
 class MyApp < Swaggering
   self.configure do |config|
-    config.api_version = '1.0.0' 
+    config.api_version = '1.0.0'
+    Mongoid.load!("./config/mongoid.yml")
   end
 end
 
