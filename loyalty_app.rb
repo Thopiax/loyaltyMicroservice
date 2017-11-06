d8fb463f9235f1896ed18a9ade95407a7b4a0910
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-06 10:17:48 +0000

    Using constants for status thresholds

--- loyalty_app.rb
+++ loyalty_app.rb
@@ -2,16 +2,21 @@ require './lib/swaggering'
 require 'mongoid'
 require './lib/status'
 
+BRONZE_THRESHOLD = 0
+SILVER_THRESHOLD = 10
+GOLD_THRESHOLD = 25
+PLATINUM_THRESHOLD = 50
+
 # only need to extend if you want special configuration!
 class LoyaltyApp < Swaggering
   self.configure do |config|
     config.api_version = '1.0.0'
     Mongoid.load!("./config/mongoid.yml")
 
-    Status.new("bronze", 0, 1)
-    Status.new("silver", 10, 3)
-    Status.new("gold", 25, 5)
-    Status.new("platinum", 50, 10)
+    Status.new("bronze", BRONZE_THRESHOLD, 1)
+    Status.new("silver", SILVER_THRESHOLD, 3)
+    Status.new("gold", GOLD_THRESHOLD, 5)
+    Status.new("platinum", PLATINUM_THRESHOLD, 10)
   end
 end
 
