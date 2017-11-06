b44cdaac4f11bdf11515e9f6b211f6ee27bb561f
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-06 01:33:45 +0000

    Added loyalty status logic

--- api/points_api.rb
+++ api/points_api.rb
@@ -27,6 +27,7 @@ LoyaltyApp.add_route('POST', '/api/user/{userId}/points', {
   return 400 unless User.valid_user_points(params["eurosSpent"], params["isRide"])
   User.find_or_404(params["userId"]) do |user|
     user.add_points(params["eurosSpent"].to_f, params["isRide"])
+    user.save!
     200
   end
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -5,12 +5,6 @@ class User
   field :status, type: String, default: "bronze"
   field :ride_count, type: Integer, default: 0
 
-  @@status_ceil = {
-    "bronze": 10,
-    "silver": 25,
-    "gold":   50
-  }
-
   def points
     self.euros_spent.to_i
   end
@@ -21,8 +15,7 @@ class User
 
   def add_points(euros_spent, is_ride)
     self.euros_spent += euros_spent
-    self.ride_count += 1 if is_ride == "true"
-    self.save!
+    self.add_ride if is_ride == "true"
   end
 
   def self.find_or_404(user_id)
@@ -34,6 +27,18 @@ class User
 
   def self.valid_user_points(euros_spent, is_ride)
     euros_spent.present? && euros_spent.to_f > 0 && # check if euros_spent is a positive float
-       (is_ride == "false" || is_ride == "true") # check if is_ride is a boolean
+       is_ride == "false" || is_ride == "true"      # check if is_ride is a boolean
+  end
+
+  def add_ride
+    self.ride_count += 1
+    self.status = loyalty_status
+  end
+
+  def loyalty_status
+    return "bronze" if self.ride_count < 10
+    return "silver" if self.ride_count < 25
+    return "gold"   if self.ride_count < 50
+    return "platinum"
   end
 end
