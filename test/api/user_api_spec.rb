2d5fda1a56d0ef34f2e7c0dff1c0e3cb70c68092
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-06 12:59:06 +0000

    Added user endpoint tests

--- test/api/user_api_spec.rb
+++ test/api/user_api_spec.rb
@@ -1,2 +1,10 @@
+require_relative "base_api_spec"
+
 class UserApiSpec < BaseApiSpec
+  def test_post_user_works
+    setup_test_user do
+      post "/api/user"
+      assert last_response.ok?
+    end
+  end
 end
