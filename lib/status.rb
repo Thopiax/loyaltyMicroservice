bb91d3f9de00fb10267a00b1047963e9c4b8a7b5
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-06 02:32:58 +0000

    Added Status class

---
+++ lib/status.rb
@@ -0,0 +1,20 @@
+class Status
+  attr_accessor :name, :ride_threshold
+
+  @@statuses = []
+
+  def initialize(name, ride_threshold)
+    @name = name
+    @ride_threshold = ride_threshold
+
+    self.class.add_status(self)
+  end
+
+  def self.status_name(status_index)
+    @@statuses[status_index].name
+  end
+
+  def self.add_status(status)
+    @@statuses << status
+  end
+end
--- loyalty_app.rb
+++ loyalty_app.rb
@@ -1,11 +1,17 @@
 require './lib/swaggering'
 require 'mongoid'
+require './lib/status'
 
 # only need to extend if you want special configuration!
 class LoyaltyApp < Swaggering
   self.configure do |config|
     config.api_version = '1.0.0'
     Mongoid.load!("./config/mongoid.yml")
+
+    Status.new("bronze", 0)
+    Status.new("silver", 10)
+    Status.new("gold", 25)
+    Status.new("platinum", 50)
   end
 end
 
