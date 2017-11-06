6a779e68fc2c62ba993504ac2d6a6a22a36410b8
Rafael Ballestiero <rafa.ballestiero@gmail.com>
2017-11-06 10:10:10 +0000

    Added euro_to_point_rate variable to status and helpers

--- lib/status.rb
+++ lib/status.rb
@@ -1,22 +1,35 @@
 class Status
-  attr_accessor :name, :ride_threshold
+  attr_accessor :name, :ride_threshold, :euro_to_point_rate
 
   @@statuses = []
 
-  def initialize(name, ride_threshold)
+  def initialize(name, ride_threshold, euro_to_point_rate)
     @name = name
     @ride_threshold = ride_threshold
+    @euro_to_point_rate = euro_to_point_rate
 
     self.class.add_status(self)
   end
 
   def self.status_name(status_index)
-    @@statuses[status_index].name
+    self.status(status_index).name
   end
 
   def self.status_threshold(status_index)
-    @@statuses[status_index].ride_threshold
+    self.status(status_index).ride_threshold
   end
+
+  def self.status_rate(status_index)
+    self.status(status_index).euro_to_point_rate
+  end
+
+  def self.status(status_index)
+    # return the highest status in the hierarchy (last in list) if index out of bounds
+    return @@statuses[status_index] if status_index >= @@statuses.size
+    @@statuses[status_index]
+  end
+
+  private
 
   def self.add_status(status)
     @@statuses << status
